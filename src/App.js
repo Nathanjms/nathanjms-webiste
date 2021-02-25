4212fb562820d9b02b120a7d5d0d1e73a51ee0b4
Nathan James <nathanjms@outlook.com>
2021-02-25 20:08:48 +0000

    Basic Homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaSpinner } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <i class="fa fa-spinner fa-spin" style={{ fontSize: '24px' }}></i>
+        <h1>Nathan James.</h1>
         <p>
-          Nathan James.
+          <FaSpinner icon="spinner" className="spinner" />
+        </p>
+        <p>
+          Coming Soon...
         </p>
       </header>
     </div >
