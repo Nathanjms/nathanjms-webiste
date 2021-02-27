bb4f01c4da5fd31c24a80b310715559482e24803
Nathan <nathanjms@outlook.com>
2021-02-27 16:56:17 +0000

    Tidy up code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,6 @@
 import React, { useReducer } from 'react';
 import { FaDesktop, FaHome, FaMusic, FaUser, FaBars } from 'react-icons/fa'
 
-// function App() {
-//     return (
-//         <>
-//             <nav className="nav-menu">
-//                 <ul>
-//                     <li className="active"><a href="/"><FaHome /><span>Home</span></a></li>
-//                     <li><a href="#about"><FaUser /><span>About</span></a></li>
-//                     <li><a href="#skills"><FaUser /><span>Skills</span></a></li>
-//                     <li><a href="#music"><FaMusic /><span>Music</span></a></li>
-
-//                 </ul>
-//             </nav>
-//             <FaBars className="mobile-nav-toggle d-xl-none" color="white" />
-//         </>
-//     );
-// }
-
-// export default App;
-
 const li = [
     {
         link: "#home",
