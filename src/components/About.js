50dfe995cecfa4b0975a8f7e5ac8aa877fd1dd99
Nathan <nathanjms@outlook.com>
2021-02-27 17:21:12 +0000

    Changes

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,21 @@ function About() {
     return (
         <div className="container">
             <div className="intro-section">
-                <h1>Nathan James.</h1>
+                <h1>About</h1>
                 <p>
-                    Coming Soon...
+                    Welcome to my website!
+                </p>
+                <p>
+                    I'm a Junior Software Developer who's still fairly new to web development. My Job
+                </p>
+                <p>
+                    Welcome to my website!
+                </p>
+                <p>
+                    Welcome to my website!
+                </p>
+                <p>
+                    This website was mainly made to try creating a website using <a href="https://reactjs.org/">React</a>, something I've been learning in my spare time.
                 </p>
                 <button className="btn btn-primary">Click Me</button>
             </div>
