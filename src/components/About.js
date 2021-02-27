8ec7a95c9897cefd6e5604d2d5c64a93cdb81486
Nathan <nathanjms@outlook.com>
2021-02-27 18:25:57 +0000

    Simplify about to see if it stops gh page failure

--- src/components/About.js
+++ src/components/About.js
@@ -10,41 +10,7 @@ function About() {
             <div className="intro-section">
                 <h1>About</h1>
 
-                <div style={{ paddingTop: '30px' }}>
-                    <TypeIt
-                        options={{
-                            loop: true,
-                            speed: 175,
-                        }}
-                        element={"h4"}
-                        getBeforeInit={instance => {
-                            instance
-                                .type("I'm a Junior Software Developer")
-                                .pause(750)
-                                .delete(25)
-                                .pause(250)
-                                .type("Musician")
-                                .pause(750)
-                                .delete(8)
-                                .pause(250)
-                                .type("Mathematician")
-                                .pause(250);
-                            return instance;
-                        }}
-                    />
 
-                    <div style={{ paddingTop: '25px' }}>
-                        <p>
-                            Welcome to my website!
-                        </p>
-                        <p>
-                            I'm a Junior Software Developer who's still fairly new to web development!
-                        </p>
-                        <p>
-                            This website was mostly made to try creating a website using <a href="https://reactjs.org/">React</a>, something I've been learning in my spare time.
-                        </p>
-                    </div>
-                </div>
             </div>
         </div>
     );
