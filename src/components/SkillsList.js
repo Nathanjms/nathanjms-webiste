3973a83c2a36f770f156e24f53a6a5a5f1df4524
Nathan <nathanjms@outlook.com>
2021-02-27 23:27:34 +0000

    Skills list set up

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,23 +1,59 @@
 import React from 'react';
-// import skillsData from './SkillsData.json';
+import skillsData from './SkillsData.json';
+import Swal from 'sweetalert2';
 
-function skillsList() {
+function SkillsInfo(skillName, desc) {
+    Swal.fire({
+        title: skillName,
+        text: desc
+    })
+}
+
+function SkillsList() {
+
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <div className="col-md-4 col-lg-3 col-sm-6 ">
-                    <div className="rating-card" data-id="0">
-                        <h3>PHP</h3>
-                        <div className="progress">
-                            <div className="progress-bar" style={{ width: "10%" }}>
-                                <span className="sr-only">70% Complete</span>
+                {skillsData.languages.map(element => (
+
+                    <div key={element.id} className="col-md-4 col-lg-3 col-sm-6 mb-1">
+                        <div className="rating-card" onClick={() => SkillsInfo(element.name, element.description)} data-id="0">
+                            <h3>{element.name}</h3>
+                            <div className="progress">
+                                <div className="progress-bar" style={{ width: (element.rating * 10) + "%" }}>
+                                    <span className="sr-only">70% Complete</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                ))}
+            </div>
+        </div >
+    );
+}
+
+function AdditionalSkillsList() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                {skillsData.additional.map(element => (
+
+                    <div key={element.id} className="col-md-4 col-lg-3 col-sm-6 ">
+                        <div className="rating-card" onClick={() => SkillsInfo(element.name, element.description)} data-id="0">
+                            <h3>{element.name}</h3>
+                            <div className="progress">
+                                <div className="progress-bar" style={{ width: (element.rating * 10) + "%" }}>
+                                    <span className="sr-only">70% Complete</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
 }
 
-export default skillsList;
+export { AdditionalSkillsList };
+
+export default SkillsList;
