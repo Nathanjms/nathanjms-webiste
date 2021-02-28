415dc4f868cfe5b73bd92c8e96986ce9cf3c25c1
Nathan <nathanjms@outlook.com>
2021-02-28 11:03:14 +0000

    Reorder skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,15 @@ function Skills() {
                 <div>
                     <h1>Skills</h1>
                     <p>Click any of the ratings below to see a bit more information about my experience with each language!</p>
-                    <SkillsList skillsData={skillsData.languages} />
+
+                </div>
+                <div className="pt-5">
+                    <h3 className="pb-3">Web Development</h3>
+                    <SkillsList skillsData={skillsData.languages.web} />
+                </div>
+                <div className="pt-5">
+                    <h3 className="pb-3">Programming</h3>
+                    <SkillsList skillsData={skillsData.languages.other} />
                 </div>
                 <div className="pt-5">
                     <h3 className="pb-3">Additional Skills</h3>
