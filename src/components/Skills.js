bf6af934769324ba51914b73168ede6a3cf35967
Nathan <nathanjms@outlook.com>
2021-02-28 10:54:39 +0000

    Pass data to make code drier

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import SkillsList, { AdditionalSkillsList } from './SkillsList'
+import SkillsList from './SkillsList';
+import skillsData from '../data/SkillsData.json';
 
 function Skills() {
     return (
@@ -8,11 +9,11 @@ function Skills() {
                 <div>
                     <h1>Skills</h1>
                     <p>Click any of the ratings below to see a bit more information about my experience with each language!</p>
-                    <SkillsList />
+                    <SkillsList skillsData={skillsData.languages} />
                 </div>
                 <div className="pt-5">
                     <h3 className="pb-3">Additional Skills</h3>
-                    <AdditionalSkillsList />
+                    <SkillsList skillsData={skillsData.additional} />
                 </div>
             </div>
         </div>
