2190d6274087c710f35bf44eb2c842d4464919f4
Nathan James <nathanjms@outlook.com>
2021-03-27 21:18:49 +0000

    pass actual userID through

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../contexts/AuthContext";
 import { Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { Card, Button } from "react-bootstrap";
@@ -8,7 +8,7 @@ import { Card, Button } from "react-bootstrap";
 export default function Movies() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
-  // const { currentUser } = useAuth();
+  const { currentUser } = useAuth();
 
   useEffect(() => {
     getMovies();
@@ -17,20 +17,21 @@ export default function Movies() {
 
   const getMovies = async () => {
     setLoading(true);
-    console.log("getting moviues");
     const result = await axios.get("http://localhost:8000/api/movies?limit=20");
     setMovies(result.data);
     setLoading(false);
   };
 
-  const markAsSeen = async (movieId, userId) => {
-    console.log("function");
-    console.log("trying");
-    await axios.post("http://localhost:8000/api/movies/mark-seen", {
-      movieId: movieId,
-      userId: userId,
-    });
-    getMovies();
+  const markAsSeen = async (movieId) => {
+    try {
+      await axios.post("http://localhost:8000/api/movies/mark-seen", {
+        movieId: movieId,
+        userId: currentUser.uid,
+      });
+      getMovies();
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
@@ -45,20 +46,22 @@ export default function Movies() {
       <div className="row">
         <div className="col-lg-12">
           <h1 className="text-center pb-5">Movies</h1>
-          <div className="w-100 text-center mt-2 row margin-auto">
+        </div>
+        <div className="col-lg-12 d-flex justify-content-center">
+          <div className="w-100 text-center mt-2 row justify-content-center">
             {loading && (
-              <div className="col-lg-12">
-                <h3>Loading...</h3>
+              <div className="col-lg-12 d-flex justify-content-center">
+                <h3>Loading Movies...</h3>
               </div>
             )}
             {movies.map((movie, index) => {
               return (
-                <div key={movie.id} className="col-md-6 col-lg-4">
-                  <Card.Body>
+                <div key={movie.id} className="col-sm-6 col-lg-4 mb-5">
+                  <Card.Body className="h-100">
                     <p>{movie.title}</p>
                     <Button
                       disabled={movie.seen}
-                      onClick={() => markAsSeen(movie.id, 1)}
+                      onClick={() => markAsSeen(movie.id)}
                     >
                       Seen It!
                     </Button>
