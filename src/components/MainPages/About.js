8456bb7b082903791c4e24e03ca216717c3f3f90
Nathan James <nathanjms@outlook.com>
2021-11-20 13:23:15 +0000

    Move info for movies section to a sweet alert

--- src/components/MainPages/About.js
+++ src/components/MainPages/About.js
@@ -1,7 +1,51 @@
 import React from "react";
 import TypeIt from "typeit-react";
+import Swal from "sweetalert2";
 
 function About() {
+  const aboutMovies = () => {
+    Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-primary swalBtn",
+        cancelButton: "btn btn-secondary swalBtn",
+      },
+      buttonsStyling: false,
+    }).fire({
+      title: "What is the movies section?",
+      html: `<div>
+        <p>In the movies section, a user can:</p>
+        <ul class="text-left">
+          <li>Create a watchlist of movies for themselves and their group.</li>
+          <li>Everyone in the group will be able to add movies, mark them as watched, as well as give them a rating!</li>
+          <li>
+            There's also a random movie picker that will choose a movie from the user's list for them - no more spending an 
+            hour deciding what to watch!
+          </li>
+        </ul>
+        <p>
+          This new API is written in PHP using
+          <a
+            href="https://laravel.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Laravel</a>, with a (pretty basic) authentication system. It's hosted on Heroku, and the code can be found
+          <a
+            href="https://github.com/Nathanjms/nathan-laravel-api"
+            target="_blank"
+            rel="noopener noreferrer"
+          >here</a>.
+        </p>
+        </div>`,
+      showCancelButton: true,
+      confirmButtonText: `Take me to the Movies Section!`,
+      cancelButtonText: `Okay`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = `https://movies.nathanjms.co.uk`
+      }
+    })
+  };
+
   return (
     <div className="container">
       <div className="intro-section">
@@ -60,34 +104,10 @@ function About() {
               .
             </p>
             <p>
-              My website now features an updated api for the{" "}
-              <a href="https://movies.nathanjms.co.uk">Movies section</a> - a
-              section where a user can create a watchlist of movies for
-              themselves and their group. Everyone in the group will be able to
-              add movies, mark them as watched, as well as give them a rating!
-              There's also a random movie picker that will choose a movie from
-              the user's list for them - no more spending an hour deciding what
-              to watch!.
-            </p>
-            <p>
-              This new API is written in PHP using{" "}
-              <a
-                href="https://laravel.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Laravel
-              </a>
-              , with a (pretty basic) authentication system. It's hosted on
-              Heroku, and the code can be found{" "}
-              <a
-                href="https://github.com/Nathanjms/nathan-laravel-api"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                here
-              </a>
-              .
+              My website now features an updated API for the Movies section.{" "}
+              <span className="link" onClick={() => aboutMovies()} tabIndex="0">
+                Read more about it here!
+              </span>
             </p>
           </div>
         </div>
